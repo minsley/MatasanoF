4cac256677df7fbeb8f9007a324dfe7752ab1c15
Matt Insley <insley.matthew@gmail.com>
2015-04-15 16:33:49 -0500

    Move shit around and start Matasano

---
+++ Convert.fs
@@ -0,0 +1,27 @@
+﻿namespace MatasanoF
+
+module Convert =
+
+    let parseHex s = System.Byte.Parse(s, System.Globalization.NumberStyles.AllowHexSpecifier)
+    let printHex (byte:byte) = System.String.Format("{0:x2}", byte)
+
+    let parseBase64 s = System.Convert.FromBase64String(s)
+    let printBase64 bytes = System.Convert.ToBase64String(bytes)
+
+    let rec chunkStr (s:string) (i:int) : array<string> = 
+        match s.Length < i with
+        | true -> [||]
+        | false -> Array.append [|s.[0..i]|] (chunkStr s.[i..] i)
+
+    let rec chunkArray (a:array<'T>) i = 
+        match a.Length < i with
+        | true -> [||]
+        | false -> Array.append [|a.[0..i]|] (chunkArray a.[i..] i)
+
+    let hexStringToBytes s = 
+        chunkStr s 2 
+        |> Array.map parseHex
+
+    let bytesToHexString (h:byte[]) = 
+        Array.map printHex h 
+        |> String.concat System.String.Empty
---
+++ MatasanoF.Test/Set01.fs
@@ -0,0 +1,23 @@
+﻿module Set01
+
+open NUnit.Framework
+open FsUnit
+open MatasanoF.Convert
+
+    (*
+
+    1.1 Hex to base64
+
+    *)
+
+    let Challenge01 = 
+
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+        let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+        let result = 
+            input
+            |> hexStringToBytes 
+            |> printBase64
+
+        printfn "%s" result
